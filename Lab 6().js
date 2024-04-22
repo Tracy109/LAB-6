41d8975309ae6d52bb9cd3685b2ec430ca6ab1be
Tracy109 <157161013+Tracy109@users.noreply.github.com>
2024-04-22 00:56:50 -0400

    Add files via upload

---
+++ Lab 6().js
@@ -0,0 +1,25 @@
+let myNumber = 10;
+let myString = "Hello, JavaScript";
+
+console.log(myNumber);
+console.log(myString);
+
+for (let i = 0; i <=10; i++) {
+console.log(i);
+}
+
+for (let i= 1; i<=5; i++) 
+console.log(3 * i);
+
+
+function addnumbers(num1, num2) {
+return num1 + num2;
+}
+
+console.log(addnumbers(5, 3));
+
+function multiplynumbers(num1, num2) {
+return num1 * num2;
+}
+
+console.log(multiplynumbers(4, 6));
